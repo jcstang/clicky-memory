a80fdf9fe48e54f64b2d9b04ea945d13c9378d0f
Jacob Stanger <jstangwork@gmail.com>
2020-05-04 17:06:05 -0600

    getting diff error now. cant read cardKey of undefined

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -82,24 +82,24 @@ class Board extends Component {
                 <div className="container">
                     {/* First Row */}
                     <div className="row">
-                        { this.renderCard(0, this.currCardList[0]) }
-                        { this.renderCard(1, this.currCardList[1]) }
-                        { this.renderCard(2, this.currCardList[2]) }
-                        { this.renderCard(3, this.currCardList[3]) }
+                        { this.renderCard(0) }
+                        { this.renderCard(1) }
+                        { this.renderCard(2) }
+                        { this.renderCard(3) }
                     </div>
                     {/* Second Row */}
                     <div className="row">
-                        { this.renderCard(4, this.currCardList[4]) }
-                        { this.renderCard(5, this.currCardList[5]) }
-                        { this.renderCard(6, this.currCardList[6]) }
-                        { this.renderCard(7, this.currCardList[7]) }
+                        { this.renderCard(4) }
+                        { this.renderCard(5) }
+                        { this.renderCard(6) }
+                        { this.renderCard(7) }
                     </div>
                     {/* Third Row */}
                     <div className="row">
-                        { this.renderCard(8, this.currCardList[8]) }
-                        { this.renderCard(9, this.currCardList[9]) }
-                        { this.renderCard(10, this.currCardList[10]) }
-                        { this.renderCard(11, this.currCardList[11]) }
+                        { this.renderCard(8) }
+                        { this.renderCard(9) }
+                        { this.renderCard(10) }
+                        { this.renderCard(11) }
                     </div>
                 </div>
 
